87bd8f0fe26c36137e31d924b76f3d9900db0848
Mario <vcmario3@gmail.com>
2021-02-02 15:02:37 +0100

    Change test config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  roots: ["."],
-  moduleFileExtensionss: ["ts", "tsx", "js", "jsx", "json", "node"],
+  roots: ["./tests/"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
-  testRegex: "./tests/.*.ts$",
+  testRegex: "./.*.ts$",
 };
